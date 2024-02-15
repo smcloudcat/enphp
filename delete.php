afa7592e701e503512c3bdcaf11b0af4b7df9cb0
云猫 <3522934828@qq.com>
2024-02-15 14:58:28 +0000

    1.3（美化页面，增加后台）
    Signed-off-by: 云猫 <3522934828@qq.com>

--- delete.php
+++ delete.php
@@ -1,19 +1,8 @@
 <?php
-// 获取请求中的密码
-$password = isset($_GET['password']) ? $_GET['password'] : '';
-
-// 检查密码是否正确
-if ($password !== '123456') {
-    $response = array(
-        'code' => 2,
-        'msg' => '密码错误'
-    );
-    echo json_encode($response);
-    exit;
-}
-
-// 指定要删除的文件夹路径
-$folderPath = 'enphp/';
+$credentials = json_decode(file_get_contents('../data/admin_credentials.json'), true);
+ if ($_POST['username'] === $credentials['username'] && md5(md5($_POST['password'])) === $credentials['password']) {
+        $_SESSION['admin'] = true;
+        $folderPath = 'enphp/';
 
 // 检查文件夹是否存在
 if (!file_exists($folderPath) || !is_dir($folderPath)) {
@@ -39,3 +28,11 @@ $response = array(
     'msg' => '删除成功'
 );
 echo json_encode($response);
+    } else {
+     $response = array(
+        'code' => 2,
+        'msg' => '密码错误'
+    );
+    echo json_encode($response);
+    exit;
+    }
