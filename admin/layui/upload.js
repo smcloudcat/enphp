09c9bc1d0a70c2b8366db7b20a875a1cf4be9ba5
云猫 <77085867+smcloudcat@users.noreply.github.com>
2024-07-29 20:16:42 +0800

    补充文件

---
+++ admin/layui/upload.js
@@ -0,0 +1,26 @@
+layui.use(['upload', 'element'], function(){
+              var upload = layui.upload;
+              var element = layui.element;
+              var uploadInst = upload.render({
+                elem: '#upload', 
+                url: 'api.php', 
+                accept: 'file', 
+                exts: 'php', 
+                before: function(obj){
+                  $('#progressBar').show();
+                },
+                progress: function(n, elem){
+                  element.progress('progressBar', n + '%');
+                },
+                done: function(res){
+                  console.log(res);
+                  $('#fileList').append('<tr><td>'+res.filename+'</td><td>'+res.result+'</td><td><a href="'+res.url+'" target="_blank">下载</a></td></tr>');
+
+                  $('#progressBar').hide();
+                },
+                error: function(){
+
+                  $('#progressBar').hide();
+                }
+              });
+            });
